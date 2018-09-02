a16ddd2401482098159ad9dc6a575612187fb52f
ivoliv <ivan.oliveira@sas.com>
2018-09-02 14:40:10 -0400

    settings

---
+++ settings_example.py
@@ -0,0 +1,19 @@
+
+
+emb_dim = 200
+hidden_dim = 300
+num_linear = 3
+
+batch_size = 16
+
+# LM settings:
+window_size=201
+lines=500
+
+
+org_data_path = '/Users/ivoliv/AI/insera/data/SpeechLabelingService'
+ORIG_DATA = 2  # 0: original data, 1: IMDB dataset, 2: wikitext
+
+imdb_file = '/Users/ivoliv/Datasets/imbd_csv/movie_reviews.csv'
+
+
