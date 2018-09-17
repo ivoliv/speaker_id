1085952f83d3420cf09cc5dc2f68a0d07944f073
ivoliv <ivan.oliveira@sas.com>
2018-09-16 22:11:57 -0700

    a

--- model/neural.py
+++ model/neural.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch
 import pdb
 
 class lang_model_LSTM(nn.Module):
@@ -57,6 +58,73 @@ class lang_model_LSTM(nn.Module):
         #    conc = out[0,:,self.hidden_dim]
 
         output = self.dropout(out)
+
+        output = self.fc(output)
+
+        # sm dims: [batch size, n_classes]
+        # sm = F.log_softmax(output, dim=-1)
+        # return sm
+
+        return output
+
+
+
+class class_model_LSTM(nn.Module):
+    def __init__(self, vocab_dim, emb_dim, hidden_dim, n_layers=1,
+                 dropout=0, bidirectional=True, n_classes=2):
+        super().__init__()
+
+        self.hidden_dim = hidden_dim
+        self.ndir = (2 if bidirectional == True else 1)
+
+        self.embedding = nn.Embedding(vocab_dim, emb_dim)
+
+        self.lstm = nn.LSTM(emb_dim, hidden_dim, num_layers=n_layers,
+                            bidirectional=bidirectional, dropout=dropout)
+
+        self.fc = nn.Linear(self.ndir * hidden_dim, n_classes)
+
+        self.dropout = nn.Dropout(dropout)
+
+        self.init_weights()
+
+    def init_weights(self, initrange=0.1):
+
+        self.embedding.weight.data.uniform_(-initrange, initrange)
+
+        for name, param in self.lstm.named_parameters():
+            if 'bias' in name:
+                param.data.zero_()
+            elif 'weight' in name:
+                param.data.uniform_(-initrange, initrange)
+
+        self.fc.weight.data.uniform_(-initrange, initrange)
+        self.fc.bias.data.zero_()
+
+    def forward(self, seq):
+
+        # pdb.set_trace()
+        # seq dims: [seq len, batch size]
+
+        emb = self.embedding(seq)
+        # emb dims: [seq len, batch size, emb dim]
+
+        out, (hid, cel) = self.lstm(emb)
+
+        # out dims: [seq len, batch size, hidden_dim]
+        # hid dims: [2*n_layers, batch size, hidden_dim]
+        # cel dims: [2*n_layers, batch size, hidden_dim]
+        # out[-1,:,:hd] -> [batch size, hidden_dim]  (last time step hidden vector)
+        # out[0,:,hd:] <- [batch size, hidden_dim]  (first time step hidden vector)
+        # contatenation of last time period, whole batch, forward (first) chunck of hidden units
+        #   and the first time period, whole batch, backward (last) chunck of hidden units
+        #   (pytorch concatenates hidden units across dim #2 for bidirectional LSTM)
+        if self.ndir == 2:
+            conc = torch.cat((out[-1,:,:self.hidden_dim], out[0,:,self.hidden_dim:]), dim=1)
+        else:
+            conc = out[0,:,self.hidden_dim]
+
+        output = self.dropout(conc)
 
         output = self.fc(output)
 
---
+++ utils/imdb_data.py
@@ -0,0 +1,183 @@
+from utils.data_import import Vocab
+from torch.utils.data import Dataset
+import torch
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import pdb
+
+class ITokListFrame():
+
+    def __init__(self, ilist_df):
+
+        self.itoklist_df = ilist_df
+        self.batch_matrix = None  # Need to call batchify
+        self.batch_start_end = None  # Need to call batchify
+
+    def __len__(self):
+
+        return self.itoklist_df.shape[0]
+
+    def show_itoklist(self, to=5):
+
+        i = 0
+        for idx, itok in self.itoklist_df.iterrows():
+            i += 1
+            print('## sentiment:', itok[0])
+            print('## text:     ', itok[1])
+            print()
+            if i >= to:
+                break
+        return
+
+    def show_stoklist(self, vocab, to=5):
+
+        i = 0
+        for idx, itok in self.itoklist_df.iterrows():
+            i += 1
+            print('## sentiment:', itok[0])
+            print('## text:     ', end=' ')
+            for w in itok[1]:
+                print(vocab.itos[w], end=' ')
+            print('\n', flush=True)
+            if i >= to:
+                break
+        return
+
+    def batchify(self, vocab, batch_size, seq_length=70):
+
+        seq_len = seq_length
+
+        nbatches = len(self.itoklist_df) // batch_size
+        print(' Number of batches:', nbatches)
+        batch_df = self.itoklist_df[:nbatches * batch_size]
+
+        print(' Preserved reviews:', batch_df.shape[0])
+        list_of_reviews = []
+
+        for idx, row in batch_df.iterrows():
+
+            row_array = row['review'][:seq_len]
+            for padding in range(len(row_array), seq_len):
+                row_array.append(vocab.stoi['<pad>'])
+            row_array.append(int(row['sentiment']))
+
+            list_of_reviews.append(row_array)
+
+        self.batch_matrix = torch.tensor(list_of_reviews).view(batch_size, -1).t_()
+        print(' Matrix size:      ', self.batch_matrix.size())
+
+        end = 0
+        self.batch_start_end = []
+        for start in range(0, self.batch_matrix.size()[0], seq_len+1):
+            end = start + seq_len+1
+            self.batch_start_end.append([start, end])
+
+        return
+
+    def batch_stats(self):
+
+        df = pd.DataFrame(self.batch_start_end)
+        df['len'] = df[1] - df[0]
+        print(df['len'].describe())
+
+        return df['len']
+
+class ImdbCorpus():
+    def __init__(self, filename='./movie_reviews.csv', lines=100, vocab_file=''):
+        self.vocab = Vocab()
+        self.imported_vocab = False
+
+        self.classes = Vocab()
+
+        if vocab_file != '':
+            self.vocab.import_vocab(vocab_file)
+            print('Imported vocab:  {:,}'.format(len(self.vocab)))
+            self.imported_vocab = True
+
+        tok, l = self.tokenize(filename, lines)
+        print('Read total of: {:,} lines from imdb file.'.format(l), flush=True)
+
+        self.n_classes = len(self.classes)
+        print('Number of classes:', self.n_classes, end=': ')
+
+        print(self.classes.stoi)
+
+        train, valid = train_test_split(tok, train_size=.7, test_size=.3)
+        self.train = ITokListFrame(train)
+        print('Generated train: {:,} lines'.format(len(self.train)), flush=True)
+
+        valid, test = train_test_split(valid, train_size=.5, test_size=.5)
+        self.valid = ITokListFrame(valid)
+        print('Generated valid: {:,} lines'.format(len(self.valid)), flush=True)
+
+        self.test = ITokListFrame(test)
+        print('Generated test:  {:,} lines'.format(len(self.test)), flush=True)
+
+        if self.imported_vocab == False:
+            print('Generated vocab: {:,}'.format(len(self.vocab)))
+
+    def tokenize(self, filename, lines):
+
+        if lines == 0:
+            df = pd.read_csv(filename)
+        else:
+            df = pd.read_csv(filename, nrows=lines)
+
+        self.vocab.add_token('<pad>')
+        self.vocab.add_token('<eol>')
+        self.vocab.add_token('<unk>')
+        self.vocab.add_token('<upcase>')
+
+        tuplelist = []
+
+        for r in df.iterrows():
+            text = (r[1][0].replace('/><br', ' ')\
+                    .replace('/>', ' ').replace('.', ' .') + ' <eol> ').strip()
+            sent = r[1][1].strip()
+
+            ilist = []
+            for char in text.split():
+                lchar = char.lower()
+                if char != lchar:
+                    ilist.append(self.vocab.add_token('<upcase>'))
+                ilist.append(self.vocab.add_token(lchar))
+
+            tuplelist.append((self.classes.add_token(sent),
+                              ilist))
+
+        return pd.DataFrame(tuplelist, columns=['sentiment', 'review']), len(tuplelist)
+
+    def batchify(self, batch_size, seq_length=70):
+
+
+        print('Batch size:       ', batch_size)
+        print('Sequence length:  ', seq_length)
+
+        print('Batchifying train...')
+        self.train.batchify(self.vocab, batch_size, seq_length)
+        print('Batchifying valid...')
+        self.valid.batchify(self.vocab, batch_size, seq_length)
+        print('Batchifying test... ')
+        self.test.batchify(self.vocab, batch_size, seq_length)
+
+        return
+
+
+class ImdbTextDataset(Dataset):
+
+    def __init__(self, itoklistFrame):
+        self.itoklistFrame = itoklistFrame
+
+    def __len__(self):
+        return len(self.itoklistFrame.batch_start_end)
+
+    def __getitem__(self, idx):
+        # pdb.set_trace()
+
+        start = self.itoklistFrame.batch_start_end[idx][0]
+        end = self.itoklistFrame.batch_start_end[idx][1]
+
+        x = self.itoklistFrame.batch_matrix[start:end-1, :]
+        y = self.itoklistFrame.batch_matrix[end-1, :]
+
+        return x, y
